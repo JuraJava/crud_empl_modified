71bb1c51ab0124e4cfdb5e65363c3f10434dde08
Jura Danilov <jura.danilov.2024@gmail.com>
2024-12-01 22:42:03 +0300

    Initial commit

--- src/main/java/com/hstn/crud_empl_modified/dao/EmployeeRepository.java
+++ src/main/java/com/hstn/crud_empl_modified/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.hstn.crud_empl_modified.dao;
 
 import com.hstn.crud_empl_modified.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "empls")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // В дженериках указывается класс сущности (из пакета entity)
     // и тот тип который является первичным ключом, т.е. Integer (т.к. int)
